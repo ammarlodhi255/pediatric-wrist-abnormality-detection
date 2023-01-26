947c2306b076360037b78b7b7303fb31f0b90f86
ammarlodhi255 <ammarlodhi68@gmail.com>
2023-01-27 02:53:48 +0500

    modified

--- draw2.py
+++ draw2.py
@@ -3,7 +3,8 @@ import numpy as np
 
 class_names = ["boneanomaly", "bonelesion", "foreignbody", "fracture",
                "metal", "periostealreaction", "pronatorsign", "softtissue", "text"]
-colors = np.random.uniform(0, 255, size=(len(class_names), 3))
+colors = [[255, 0, 0], [0, 255, 0], [255, 178, 29], [255, 178, 29], [
+    207, 210, 49], [71, 249, 10], [255, 128, 0], [26, 147, 52], [26, 147, 52]]
 
 
 def draw_boxes(image_path, txt_path, output_path):
@@ -25,20 +26,24 @@ def draw_boxes(image_path, txt_path, output_path):
 
         xmin = int((x - w/2) * image.shape[1])
         ymin = int((y - h/2) * image.shape[0])
+
         xmax = int((x + w/2) * image.shape[1])
         ymax = int((y + h/2) * image.shape[0])
+
 
         color = tuple(colors[class_id])
         cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
 
         class_name = class_names[class_id]
-        cv2.putText(image, class_name, (xmin, ymin - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-        cv2.rectangle(image, (xmin, ymin-30), (xmin+80, ymin), color, -1)
-        cv2.putText(image, class_name, (xmin, ymin - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-
-    # Save the output image
+    # Get the size of the text
+        (text_width, text_height), _ = cv2.getTextSize(
+        class_name, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
+    # Draw the text rectangle
+        cv2.rectangle(image, (xmin, ymin-text_height-20),
+                  (xmin+text_width+20, ymin), color, -1)
+        cv2.putText(image, class_name, (xmin+10, ymin-10),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+       
     cv2.imwrite(output_path, image)
     print("Output image saved to", output_path)
 
